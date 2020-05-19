e01730a578c2507188965917ea392568c2b3e863
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-05-20 01:20:47 +0530

    News api

--- apis/routes.py
+++ apis/routes.py
@@ -14,22 +14,102 @@ from Summariser import create_summary
 from Real_Time_Research_News import get_news
 
 
+@app.route("/api/register", methods=['POST'])
+def register():
+    user_data = request.get_json()
+    email = user_data['email']
+    name = user_data['name']
+    password = bcrypt.generate_password_hash(user_data['password']).decode('utf-8')
+    profession = user_data['profession']
+    if not email:
+        return jsonify({'error': 'Missing email'}), 400
+    if not name:
+        return jsonify({'error': 'Missing name'}), 400
+    if not password:
+        return jsonify({'error': 'Missing password'}), 400
+    if not profession:
+        return jsonify({'error': 'Missing profession'}), 400
+
+    try:
+        user = User(email=email, name=name, profession=profession, password=password)
+        db.session.add(user)
+        db.session.commit()
+        token = create_access_token(identity={'name': name, 'email': email})
+        data = {
+            'token': token,
+            'email': email,
+            'name': name,
+            'profession': profession,
+        }
+        return jsonify({'data': data}), 200
+    except:
+        return jsonify({'error': 'User already exists'}), 401
+
+
+@app.route("/api/login", methods=['POST'])
+def login():
+    user_data = request.get_json()
+    email = user_data['email']
+    password = user_data['password']
+    if not email:
+        return jsonify({'error': 'Missing email'}), 400
+    if not password:
+        return jsonify({'error': 'Missing password'}), 400
+    try:
+        object_data = User.query.filter_by(email=email).first()
+        check_password = object_data.password
+        if bcrypt.check_password_hash(check_password, password):
+            token = create_access_token(identity={'name': object_data.name, 'email': object_data.email})
+            data = {
+                'token': token,
+                'email': object_data.email,
+                'name': object_data.name,
+                'profession': object_data.profession,
+            }
+            return jsonify({'data': data}), 200
+        return jsonify({'error': 'Password is wrong'}), 401
+    except:
+        return jsonify({'error': 'User does not exist'}), 401
+
+
 @app.route("/api/news", methods=['GET'])
-@cross_origin()
 def news():
     data_frame = get_news()
     data_frame = data_frame.drop(columns=['Datetime'])
     info = []
     objects = {}
     for i in range(data_frame.shape[0]):
-        objects['Links'] = data_frame['Links'][i]
         objects['Headlines'] = data_frame['Headlines'][i]
         objects['Publisher'] = data_frame['Publisher'][i]
+        objects['Links'] = data_frame['Links'][i]
         objects['Hours'] = data_frame['Hours'][i]
         info.append(objects)
         objects = {}
     return jsonify({'data': info}), 200
 
+
+@app.route("/api/postnews", methods=['POST'])
+def postnews():
+    post_data = request.get_json()
+    query = post_data['headline']
+    data_frame = get_news()
+    text = data_frame['Headlines']
+    text = text.tolist()
+    abstract_vectors = generate_embeddings(text, word_embeddings, data_frame)
+    result = cosine_sim(query, word_embeddings, abstract_vectors, data_frame)
+    result = result.drop(columns=['Datetime', 'Cosine'])
+    l = []
+    info = []
+    objects = {}
+    l = result.index.values
+    for i in range(10):
+        objects['Links'] = result['Links'][l[i]]
+        objects['Headlines'] = result['Headlines'][l[i]]
+        objects['Publisher'] = result['Publisher'][l[i]]
+        objects['Hours'] = result['Hours'][l[i]]
+        info.append(objects)
+        objects = {}
+    return jsonify({'data': info}),200 200                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 
 @app.route("/api/summarise", methods=['POST'])
 @cross_origin()
@@ -80,68 +160,6 @@ def info_retrieval():
         info.append(objects)
         objects = {}
     return jsonify({'data': info}), 200
-
-
-@app.route("/api/register", methods=['POST'])
-@cross_origin()
-def register():
-    user_data = request.get_json()
-    email = user_data['email']
-    name = user_data['name']
-    password = bcrypt.generate_password_hash(user_data['password']).decode('utf-8')
-    profession = user_data['profession']
-
-    if not email:
-        return jsonify({'error': 'Missing email'}), 400
-    if not name:
-        return jsonify({'error': 'Missing name'}), 400
-    if not password:
-        return jsonify({'error': 'Missing password'}), 400
-    if not profession:
-        return jsonify({'error': 'Missing profession'}), 400
-
-    if User.query.filter_by(email=email).first() != email or User.query.filter_by(email=email).first() is None:
-        user = User(email=email, name=name, profession=profession, password=password)
-        db.session.add(user)
-        print("HI")
-        db.session.commit()
-        token = create_access_token(identity={'name': name, 'email': email})
-        data = {
-            'token': token,
-            'email': email,
-            'name': name,
-            'profession': profession,
-        }
-        return jsonify({'data': data}), 200
-    else:
-        return jsonify({'error': 'User already exists'}), 401
-
-
-@app.route("/api/login", methods=['POST'])
-@cross_origin()
-def login():
-    user_data = request.get_json()
-    email = user_data['email']
-    password = user_data['password']
-    if not email:
-        return jsonify({'error': 'Missing email'}), 400
-    if not password:
-        return jsonify({'error': 'Missing password'}), 400
-    try:
-        object_data = User.query.filter_by(email=email).first()
-        check_password = object_data.password
-        if bcrypt.check_password_hash(check_password, password):
-            token = create_access_token(identity={'name': object_data.name, 'email': object_data.email})
-            data = {
-                'token': token,
-                'email': object_data.email,
-                'name': object_data.name,
-                'profession': object_data.profession,
-            }
-            return jsonify({'data': data}), 200
-        return jsonify({'error': 'Password is wrong'}), 401
-    except:
-        return jsonify({'error': 'User does not exist'}), 401
 
 
 @app.route('/api/mysummaries', methods=['GET'])
